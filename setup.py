7e975cedbb730902ccd84acea585b60c0b210632
root <root@evalon.kmdi.utoronto.ca>
2010-06-17 13:24:42 -0400

    Distutils installer

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='portal',
+    version='1.0',
+    description='Evaluation portal',
+    author='Velian Pandeliev',
+    packages=['portal', 'portal.studies'],
+    )
