e6ff94aecac6b1218501e93c63619dec985c7121
ldk <liam.kaufman@gmail.com>
2010-07-08 10:52:26 -0400

    removed lockdown and tinymce

--- portal/django-tinymce-1.5/metadata.py
+++
@@ -1,4 +0,0 @@
-name = 'tinymce'
-authors = 'Joost Cassee'
-version = '1.5'
-release = version
--- portal/lockdown/decorators.py
+++
@@ -1,5 +0,0 @@
-from django.utils.decorators import decorator_from_middleware
-
-from lockdown.middleware import LockdownMiddleware
-
-lockdown = decorator_from_middleware(LockdownMiddleware)
