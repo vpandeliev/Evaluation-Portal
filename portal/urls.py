5c7cc5784dab0d6b8cb800f08cdec4d3b6bb50f0
velian <vpandeliev@gmail.com>
2011-03-03 10:23:37 -0500

    setswitching updated

--- portal/urls.py
+++ portal/urls.py
@@ -24,9 +24,8 @@ urlpatterns = patterns('',
 
 
 if settings.DEBUG:
-	pass
-#	urlpatterns += patterns('',
-#	(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-#	{'document_root': settings.MEDIA_ROOT }),
-#	)
+	urlpatterns += patterns('',
+	(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+	{'document_root': settings.MEDIA_ROOT }),
+	)
 
