47f2a058d04cf422e0b0ca8379ae4d9b081ce9a0
kante <kante@lappy.local>
2011-09-10 14:43:32 -0400

    fixed the user list to use an internal cache instead of the database. This will have far superior performance.

--- portal/user_activity_middleware.py
+++ portal/user_activity_middleware.py
@@ -10,12 +10,12 @@ from studies.models import UserActivity
 from datetime import datetime
 from django.conf import settings
 from django.contrib.sites.models import Site
+from django.core.cache import cache
 
 class UserActivityMiddleware:
     def process_view(self, request, view, args, kwargs):
         if not request.user.is_authenticated():
             return
-        
         
         # Only update when the users are in a study doing stuff.
         if request.path.find("study") == -1:
@@ -31,7 +31,21 @@ class UserActivityMiddleware:
             activity.last_activity_ip = request.META['REMOTE_ADDR']
             activity.save()
             return
-            
+        
+        # TESTING: memory cache for saving active users.
+        user_list_timeout = 160000000
+        cached_users = cache.get("recent_users_list")
+        if not cached_users:
+            cache.set("recent_users_list", [request.user], user_list_timeout)
+        else:
+            cached_users.append(request.user)
+            cache.set("recent_users_list", cached_users, user_list_timeout)
+        
+        # Users time out after 30 seconds. 
+        # TODO: make this a parameter in the settings.py file!!!!!!
+        cache.set(request.user, 1, 30)
+        
+        
         activity.last_activity_date = datetime.now()
         activity.last_activity_ip = request.META['REMOTE_ADDR']
         activity.save()
--- portal/video_conferencing/views.py
+++ portal/video_conferencing/views.py
@@ -2,18 +2,40 @@ from django.http import *
 from django.contrib.auth.decorators import login_required
 from django.template import Template, Context, RequestContext
 from django.shortcuts import render_to_response
-from django.forms import Form, CharField, DateField, ChoiceField, Select, BooleanField
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+from studies.models import UserActivity
+from django.core.cache import cache
+
 
 #################################################################################
 #   DJANGO URLS
 #################################################################################
 
 
+def get_online_users(num):
+    #one_minute = datetime.now() - timedelta(minutes=1)
+    #sql_datetime = datetime.strftime(one_minute, '%Y-%m-%d %H:%M:%S')
+    #users = UserActivity.objects.filter(last_activity_date__gte=sql_datetime, user__is_active__exact=1).order_by('-last_activity_date')[:num]
+    #return [u.user for u in users]
+    
+    cached_users = cache.get("recent_users_list") or []
+    now = datetime.now()
+    
+    for user in cached_users:
+        # If user entry is no longer in the cache, then it has timed out and the
+        # user is no longer active. Update the list to reflect this
+        if not cache.get(user):
+            cached_users.remove(user)
+    # save the new user list in the cache.
+    cache.set("recent_users_list", cached_users, 16000000)
+    
+    return cached_users
 
 
 #@login_required
-def basic_test(request):    
-    test = "basbababa"
+def basic_test(request):
+    test = get_online_users(5)
     
     return render_to_response('basic_test.html', locals(), 
                               context_instance=RequestContext(request))
